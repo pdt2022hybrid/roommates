8e66d114cbd10ebc7f2dff46f025ac40828efef8
richardko1303 <help.covidrobot@gmail.com>
2023-04-19 14:36:39 +0200

    bd

--- cms/plugins/mates/room/http/resources/MiniRoomResource.php
+++ cms/plugins/mates/room/http/resources/MiniRoomResource.php
@@ -3,14 +3,14 @@
 namespace Mates\Room\Http\resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Mates\Room\Http\resources\RoomResource;
 class MiniRoomResource extends JsonResource
 {
-    public function toArray($request) {
+    public function toArray($request): array
+    {
         return [
             'id' => $this->resource->id,
             'name' => $this->resource->name,
-            'room' => new RoomResource($this->room) //TODO: toto je picovina ma tam byt RoomResource::make($this->room)
+            'room' => RoomResource::make($this->room)
         ];
     }
 }
--- cms/plugins/mates/room/http/resources/RoomResource.php
+++ cms/plugins/mates/room/http/resources/RoomResource.php
@@ -3,6 +3,7 @@
 namespace Mates\Room\Http\resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Mates\Room\Models\Miniroom;
 use Mates\User\Http\Resources\UserResource;
 class RoomResource extends JsonResource
 {
@@ -12,7 +13,7 @@ class RoomResource extends JsonResource
             'name' => $this->resource->name,
             'room_count' => $this->resource->room_count,
             'room_url' => $this->resource->room_url,
-            'room_owner' => new UserResource($this->resource->room_owner)
+            'room_owner' => UserResource::make($this->resource->room_owner),
         ];
     }
 }
