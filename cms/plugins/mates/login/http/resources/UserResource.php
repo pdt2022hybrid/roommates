9f3d4d370ce0303346c4bec8dd7e33f06ec14962
TOPOLSKY11 <topolsky.business@gmail.com>
2023-04-25 12:10:53 +0200

    KOKOT
    PIČA

--- cms/plugins/mates/login/http/resources/UserResource.php
+++ cms/plugins/mates/login/http/resources/UserResource.php
@@ -3,6 +3,8 @@
 namespace Mates\Login\Http\resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
+
 class UserResource extends JsonResource
 {
     public function toArray($request) {
@@ -12,7 +14,9 @@ class UserResource extends JsonResource
             "surname" => $this->resource->surname,
             "email" => $this->resource->email,
             "is_activated" => $this->resource->is_activated,
-            "registered_at" => date($this->resource->created_at) //TODO: pouzi Carbon
+            //"registered_at" => date($this->resource->created_at) //TODO: pouzi Carbon
+            "registered_at" => Carbon::parse($this->resource->created_at)->format('Y-m-d H:i:s'),
+            
         ];
     }
 }
