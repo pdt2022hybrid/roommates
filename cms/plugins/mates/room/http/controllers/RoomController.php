76019986d102309091325b54c25f1dff1ed80808
richardko1303 <help.covidrobot@gmail.com>
2023-04-26 09:23:52 +0200

    Update RoomController.php

--- cms/plugins/mates/room/http/controllers/RoomController.php
+++ cms/plugins/mates/room/http/controllers/RoomController.php
@@ -27,33 +27,50 @@ class RoomController extends Controller
         $room->room_owner_id = $postData['room_owner_id'];
         $room->save();
 
+        $user->room_id = $room->id;
+        $user->save();
+
+        $miniRooms = collect();
+
+        if (post('izby')) {
+            foreach (post('izba') as $name) {
+                $room->room_count++;
+                $room->save();
+                $miniRoom = new Miniroom();
+                $miniRoom->room_id = $room->id;
+                $miniRoom->name = $name;
+                $miniRoom->save();
+                $miniRooms->push($miniRoom);
+            }
+        }
+
         return new RoomResource($room);
     }
 
-    public function createMiniRoom(Request $request)
-    {
-        $room_id = User::where('id', $request->get('tokenUserID'))->first()->room_id;
-        $room = Room::where('id', $room_id)->first();
-
-        if (!$room) {
-            return response()->json([
-                'error' => 'Room not found SUPPLIED ROOM_ID: ' . $room_id
-            ], 404);
-        }
-
-        $room->room_count++;
-        $room->save();
-        $miniRooms = collect();
-
-        foreach (post('izba_name') as $name) {
-            $miniRoom = new Miniroom();
-            $miniRoom->room_id = $room_id;
-            $miniRoom->name = $name;
-            $miniRoom->save();
-            $miniRooms->push($miniRoom);
-        }
-        return MiniRoomResource::collection($miniRooms);
-    }
+//    public function createMiniRoom(Request $request)
+//    {
+//        $room_id = User::where('id', $request->get('tokenUserID'))->first()->room_id;
+//        $room = Room::where('id', $room_id)->first();
+//
+//        if (!$room) {
+//            return response()->json([
+//                'error' => 'Room not found SUPPLIED ROOM_ID: ' . $room_id
+//            ], 404);
+//        }
+//
+//        $room->room_count++;
+//        $room->save();
+//        $miniRooms = collect();
+//
+//        foreach (post('izba_name') as $name) {
+//            $miniRoom = new Miniroom();
+//            $miniRoom->room_id = $room_id;
+//            $miniRoom->name = $name;
+//            $miniRoom->save();
+//            $miniRooms->push($miniRoom);
+//        }
+//        return MiniRoomResource::collection($miniRooms);
+//    }
 
     public function getAllMiniRooms(Request $request) {
         return new MiniRoomResource(Miniroom::all());
