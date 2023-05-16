3cd84630407b812d49fa154ff8d75cec71eef507
richardko1303 <help.covidrobot@gmail.com>
2023-05-16 09:22:37 +0200

    Update routes.php

--- cms/plugins/mates/room/routes.php
+++ cms/plugins/mates/room/routes.php
@@ -9,7 +9,7 @@ use Mates\Room\Http\Controllers\RoomController;
 Route::group(['prefix' => 'api/v1'], function () {
     Route::middleware([TokenMiddleware::class])->group(function () {
         Route::post('room/create', [RoomController::class, 'createRoom']);
-        Route::post('room/join/{roomIdentifier}', [RoomController::class, 'joinRoom']);
+        Route::get('room/join/{roomIdentifier}', [RoomController::class, 'joinRoom']);
         Route::post('room/miniroom/create', [RoomController::class, 'createMiniRoom']); //TODO: naozaj by si mohol pouzit tie api standarty
         Route::get('rooms/all', [RoomController::class, 'getAllRooms']);
         Route::get('room/minirooms', [RoomController::class, 'getMiniRoomsInRoom']);
